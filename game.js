5fd63b47df8e2d1325e08d5c64bf47bd22782f3e
brandontominna <tominnabrandon363@gmail.com>
2023-09-26 01:29:14 -0700

    added food and functionality for snake to expand when food is encountered

--- game.js
+++ game.js
@@ -1,32 +1,48 @@
-import { update as updateSnake, draw as drawSnake, SNAKE_SPEED } from "./snake.js"
+import {
+  update as updateSnake,
+  draw as drawSnake,
+  SNAKE_SPEED,
+} from "./snake.js";
 
-let lastRenderTime = 0
-const gameBoard = document.getElementById('game-board')
-console.log(gameBoard)
+import {
+    update as updateFood, 
+    draw as drawFood,
+} from './food.js'
+
+let lastRenderTime = 0;
+const gameBoard = document.getElementById("game-board");
+console.log(gameBoard);
 
 function main(currentTime) {
-    window.requestAnimationFrame(main)
-    const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000
-    if (secondsSinceLastRender < 1 / SNAKE_SPEED) return
-    
-    lastRenderTime = currentTime
+  window.requestAnimationFrame(main);
+  const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
+  if (secondsSinceLastRender < 1 / SNAKE_SPEED) return;
 
-    update()
-    draw()
+  lastRenderTime = currentTime;
+
+  update();
+  draw();
 }
 
-window.requestAnimationFrame(main)
-
-
+window.requestAnimationFrame(main);
 
 function update() {
-    updateSnake()
+  updateSnake();
+  updateFood()
 }
-
-
 
 function draw() {
-    gameBoard.innerHTML = ''
-    drawSnake(gameBoard)
+  gameBoard.innerHTML = "";
+  drawSnake(gameBoard);
+  drawFood(gameBoard)
 }
+
+
+
+
+
+
+
+
+
 
--- snake.js
+++ snake.js
@@ -4,7 +4,10 @@ export const SNAKE_SPEED = 2;
 
 const snakeBody = [{ x: 11, y: 11 }];
 
+let newSegments = 0
+
 export function update() {
+  addSegments()
   const inputDirection = getInputDirection();
   for (let i = snakeBody.length - 2; i >= 0; i--) {
     snakeBody[i + 1] = { ...snakeBody[i] };
@@ -26,3 +29,26 @@ export function draw(gameBoard) {
     gameBoard.appendChild(snakeElement);
   });
 }
+
+
+export function expandSnake(amount) {
+    newSegments += amount
+}
+
+export function onSnake(position) {
+    return snakeBody.some(segment => {
+        return equalPositions(segment, position)
+    })
+}
+
+function equalPositions(pos1, pos2) {
+    return pos1.x === pos2.x && pos1.y === pos2.y
+}
+
+function addSegments() {
+  for(let i = 0; i < newSegments; i++) {
+    snakeBody.push({...snakeBody[snakeBody.length - 1]})
+  }
+}
+
+
