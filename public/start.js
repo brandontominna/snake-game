9f6d7c60c9d40b8a260ac699886792be01f6bc35
brandontominna <tominnabrandon363@gmail.com>
2023-11-11 01:13:56 -0800

    added express server and re-organized file paths to account for this

---
+++ public/start.js
@@ -0,0 +1,8 @@
+
+let menuButton = document.getElementById('startButton')
+
+menuButton.addEventListener('click', () => {
+    window.location.href = '../menuPage/menu.html'
+})
+
+
--- start.js
+++
@@ -1,7 +0,0 @@
-
-let menuButton = document.getElementById('startButton')
-
-menuButton.addEventListener('click', () => {
-    window.location.href = './menuPage/menu.html'
-})
-
